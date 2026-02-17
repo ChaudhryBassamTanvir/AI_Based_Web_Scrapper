bf416d908cb27341a8ab6915f482bd7275da0881
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2026-02-17 17:55:59 +0500

    api path updated

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,5 +1,8 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+const BASE_URL = "https://railway.com/project/26ec6265-cf89-45c7-8d08-618f6606b596?environmentId=ebc801e4-0128-410d-ba96-9ec58c45a2b7";
+
 export async function scrapeWebsite(url: string) {
-  const res = await fetch("http://localhost:8000/scrape", {
+  const res = await fetch(`${BASE_URL}/scrape`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ url }),
@@ -13,7 +16,7 @@ export async function formatScrapedData(data: any, formatType?: string) {
     payload.format_type = formatType.trim();
   }
 
-  const res = await fetch("http://localhost:8000/format", {
+  const res = await fetch(`${BASE_URL}/format`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
